75e01ed052382ab19b12ad2f3d4fddd698e6428b
I-Kouta <kouta.baseball0222@gmail.com>
2022-03-24 20:11:58 +0900

    コメントの情報が画面に表示される記述

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -12,7 +12,17 @@ if(location.pathname.match(/\/items\/\d/)){
     },
 
     received(data) {
-      console.log(data)
+      // 変数に代入,dataからキー名を指定してコメントと送信者の情報を取出
+      const html = `
+        <div class="comment">
+          <p class="user-info">${data.user.nickname}： </p>
+          <p>${data.comment.text}</p>
+        </div>`
+      const comments = document.getElementById("comments")
+      comments.insertAdjacentHTML('beforeend', html)
+      // フォームの中身をリセット
+      const commentForm = document.getElementById("comment-form")
+      commentForm.reset();
     }
   })
 }
